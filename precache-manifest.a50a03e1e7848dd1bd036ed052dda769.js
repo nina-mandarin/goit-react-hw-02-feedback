b0c6624fe0b9906b94f349ee1bf767a6b5231111
NinaMiroshnychenko <nina.m@medichi.com.ua>
2020-02-02 14:07:04 +0200

    Updates

---
+++ precache-manifest.a50a03e1e7848dd1bd036ed052dda769.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4906e491be7adcff20907f738ec45570",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "b843633c85fcafedd16a",
+    "url": "/goit-react-hw-02-feedback/static/css/main.f58222ac.chunk.css"
+  },
+  {
+    "revision": "9942abcc9ab5b93dab23",
+    "url": "/goit-react-hw-02-feedback/static/js/2.ec2c5e8d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-02-feedback/static/js/2.ec2c5e8d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b843633c85fcafedd16a",
+    "url": "/goit-react-hw-02-feedback/static/js/main.dfa2e2e1.chunk.js"
+  },
+  {
+    "revision": "4c3d7c3ac1ad4fad6b67",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.77b613c8.js"
+  }
+]);
